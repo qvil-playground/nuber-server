cab096972dffeb4b5cd71f79b1d7f17dfac3af22
Taesu Hyeon <qvil1127@gmail.com>
2018-10-03 20:48:30 +0900

    #1.9 Typechecking Graphql Arguments

--- src/api/Hello/sayHello/sayHello.resolvers.ts
+++ src/api/Hello/sayHello/sayHello.resolvers.ts
@@ -1,10 +1,10 @@
-import { Greeting } from "../../../types/graph";
+import { SayHelloQueryArgs, SayHelloResponse } from "../../../types/graph";
 
 const resolvers = {
   Query: {
-    sayHello: (): Greeting => ({
+    sayHello: (_ ,args: SayHelloQueryArgs): SayHelloResponse => ({
       error: false,
-      text: "love2"
+      text: `Hello ${args.name}`
     })
   }
 };
