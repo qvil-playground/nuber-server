2ee35f4a2f7e794c61d6b85c89fb27ee41624f18
Taesu Hyeon <qvil1127@gmail.com>
2018-11-24 04:46:56 +0900

    Edit ormConfig for elephantsql

--- src/ormConfig.ts
+++ src/ormConfig.ts
@@ -2,14 +2,15 @@ import { ConnectionOptions } from "typeorm";
 
 const connectionOptions: ConnectionOptions = {
   type: "postgres",
+  // url: process.env.DB_URL,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   synchronize: true,
   logging: true,
-  entities: ["entities/**/*.*"],
-  host: process.env.DB_ENDPOINT,
-  port: 5432,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD
+  entities: ["entities/**/*.*"]
 };
 
 export default connectionOptions;
